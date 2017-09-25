ea1c82b42cd4a3f2213062f3c8f8c15a5bd56f58
Charles Winston <charles.winston@tufts.edu>
2017-09-25 18:31:34 -0400

    Homework 2 progress

--- lab2/ngram.py
+++ lab2/ngram.py
@@ -1,43 +1,65 @@
+# ngram.py
+#   Lab 2 for COMP 150 NLP
+#   by Charles Winston
+#   9/25/17
+#
+#   Main program for Lab 2. Computes a bigram model from the Bible
+#   and compares the writings of Shakespeare and Herman Melville to
+#   the Bible based on that model. The "loser" of the prediction then
+#   gets a model made for himself, and then the remaining two works
+#   are compared to his writing the same way.
+
 from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk.corpus import gutenberg
+import models
+import predictions
+import conversation
+
+
+def losers_turn(model_text, compare_text1, compare_text2):
+    # Converts text string read by gutenberg to name to print
+    def corpusstr2name(str):
+        if (str == 'bible-kjv.txt'):
+            return "The Bible"
+        if (str == 'shakespeare-hamlet.txt'):
+            return "Shakespeare"
+        if (str == 'melville-moby_dick.txt'):
+            return "Herman Melville"
+
+    # Create model
+    model = models.bigram(gutenberg.words(model_text))
+
+    # Make predictions
+    compare1 = gutenberg.sents(compare_text1)
+    compare2 = gutenberg.sents(compare_text2)
+
+    if (predictions.prediction(model, compare1, compare2) == compare1):
+        print(corpusstr2name(compare_text1) + " can sit with " + corpusstr2name(model_text))
+    else:
+        print(corpusstr2name(compare_text2) + " can sit with " + corpusstr2name(model_text))
+
 
 def main():
-    shakespeare = gutenberg.words('shakespeare-hamlet.txt')
-    melville = gutenberg.words('melville-moby_dick.txt')
-    bible = gutenberg.words('bible-kjv.txt')
+    # Create bigram model from Bible
+    bible_model = models.bigram(gutenberg.words('bible-kjv.txt'))
 
-    words = word_tokenize("I I am am a a a sucker stupid ass ass ass ass ass I am I am I am hey.")
+    conversation.generate_sentence(bible_model)
 
-    print(bigram(words))
+    '''
 
-# Text is a list of words
-def bigram(text):
-    countTable = {}
+    # Make predictions
+    shakespeare = gutenberg.sents('shakespeare-hamlet.txt')
+    melville = gutenberg.sents('melville-moby_dick.txt')
 
-    for i in range(len(text)):
-        countTable.setdefault(text[i], {})
-        countTable[text[i]].setdefault(text[i-1], 0)
-        if (i != 0):
-            count = countTable[text[i]][text[i-1]] + 1
-            countTable[text[i]][text[i-1]] = count
+    if (predictions.prediction(bible_model, shakespeare, melville) == shakespeare):
+        print("Shakespeare can sit with the Bible")
+        losers_turn('melville-moby_dick.txt', 'shakespeare-hamlet.txt', 'bible-kjv.txt')
+    else:
+        print("Herman Melville can sit with the Bible")
+        losers_turn('shakespeare-hamlet.txt', 'melville-moby_dick.txt', 'bible-kjv.txt')
+    '''
 
-    return calcProbs(countTable, laplace)
 
-def calcProbs(countTable, smoothing):
-    vocab = len(countTable)
-    for key1 in countTable:
-        total = 0
-        # add up the totals
-        for key2 in countTable[key1]:
-            total += countTable[key1][key2]
-        # compute probabilities using Laplace smoothing
-        for key2 in countTable[key1]:
-            countTable[key1][key2] = smoothing(countTable[key1][key2], total, vocab)
-
-    return countTable
-
-def laplace(count, n, v):
-    return (count + 1) / float(n + v)
 
 if __name__ == '__main__':
     main()
